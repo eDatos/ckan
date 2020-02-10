f3a91a355916cdc7a47e81a10aadfff125f0436f
jescgom <jescgom@arte-consultores.com>
2020-02-10 11:30:14 +0000

    EDATOS-2842 - ISTAC THEME ext
    > Modificación estilos
    > Ocultada pestaña Actividad de datasets
    > Mostradas las categorías en el home
    > Añadido el botón inicio en el header
    > Mostrado el contador total de datasetsi en el home
    > Modificado el footer
    > Modificaco el header

--- ckanext/istac_theme/plugin.py
+++ ckanext/istac_theme/plugin.py
@@ -1,24 +1,43 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import os
 from ckan.lib.plugins import DefaultTranslation
+
+
+def get_all_groups():
+    '''Return a list of all the groups
+
+    '''
+    groups = toolkit.get_action('group_list')(
+        data_dict={'sort': 'package_count desc', 'all_fields': True})
+
+    return groups
+
+
+def get_count_all_datasets():
+    '''Returns the total number or datasets
+    '''
+    package_list = toolkit.get_action('package_list')(
+        data_dict={
+        })
+    count = len(package_list)
+
+    return count
 
 
 class IstacThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
     # IConfigurer
-
-    f = open("/servers/opendata/ckan/default/src/ckanext-istac_theme/ckanext/istac_theme/prueba.txt","w+")
-    for i in range(10):
-        f.write("This is line %d\r\n" % (i+1))
-    f.close() 
-
-    cwd = os.getcwd()
-    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx " + cwd)    
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'istac_theme')
- 
+
+    def get_helpers(self):
+        '''Register the get_all_groups function as a template helper function.
+        '''
+        return {'istac_theme_get_all_groups': get_all_groups,
+                'istac_theme_get_count_all_datasets': get_count_all_datasets,
+                }
